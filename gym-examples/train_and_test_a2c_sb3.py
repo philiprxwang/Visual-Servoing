17251e0688fdc0d0441d9d83ee6d32290f2e8e42
philiprxwang <123920028+philiprxwang@users.noreply.github.com>
2023-09-22 18:57:11 +0800

    Changed A2C to DQN

--- gym-examples/train_and_test_a2c_sb3.py
+++ gym-examples/train_and_test_a2c_sb3.py
@@ -2,24 +2,34 @@ import gymnasium as gym
 import gym_examples
 from gym_examples.envs.phil_env import PhilEnv
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3 import A2C
+from stable_baselines3 import DQN
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
+from DQN.wrappers import *
 
 env = gym.make("gym_examples/PhilEnv-v1", render_mode="human")
+# Frame-stacking with 4 frames using custom gym wrapper
+# env = FrameStack(env, 4)
 
 # Instantiate the agent
-model = A2C("CnnPolicy", env, verbose=1, tensorboard_log = './a2c_tensorboard_log/')
+model = DQN("CnnPolicy", 
+            env, 
+            buffer_size = 80000, 
+            learning_starts = 4000, 
+            verbose=1, 
+            tensorboard_log = './dqn_tensorboard_log/',
+            exploration_fraction = 0.5) # buffer size is 10x smaller than default because my mac doesn't have enough memory
+
 # Train the agent and display a progress bar
-model.learn(total_timesteps=int(50000), progress_bar=True)
+model.learn(total_timesteps=int(60000), progress_bar=True)
 # Save the agent
-model.save("A2C_test")
+model.save("DQN_test")
 
 # del model  # delete trained model to demonstrate loading
 
-# model = A2C.load("A2C_test", env=env)
+# model = DQN.load("DQN_test", env=env)
 
 # #mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
