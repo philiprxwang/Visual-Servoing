86f964820b3d25a8a90046adc0af569f8efdd401
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-09-25 21:08:36 +0800

    Changed reset method and created new box object

--- gym-examples/gym_examples/envs/phil_env.py
+++ gym-examples/gym_examples/envs/phil_env.py
@@ -18,7 +18,7 @@ IMG_WIDTH = 210
 IMG_MASK_THRESH = 100
 NUM_CHANNELS = 1
 NUM_FRAMES = 1
-DIR_SIZE = 0.1
+DIR_SIZE = 0.2
 
 class PhilEnv(gym.Env):
     """
@@ -38,11 +38,11 @@ class PhilEnv(gym.Env):
         self.dir_size = DIR_SIZE
         self.img_mask_thresh = IMG_MASK_THRESH
 
-        #p.connect(p.GUI)
-        p.connect(p.DIRECT) 
+        p.connect(p.GUI)
+        #p.connect(p.DIRECT) 
         self.action_space = spaces.Discrete(6) # total 6 actions: front, back, left, right, up, down
-        self.observation_space = spaces.Box(low=0, high = 255, shape = (self.num_channels, 84, 84), dtype = np.uint8) # observation, 160x210 RGB array, undergoes: grayscale, mask and resize to 84x84
-        
+        self.observation_space = spaces.Box(low=0, high = 255, shape = (self.num_channels, 84, 84), dtype = np.uint8) # observation, 160x210 RGB array, undergoes: grayscale, mask and resize to 84x84. Activate this line for SB3
+        #self.observation_space = spaces.Box(low=0, high = 255, shape = (84, 84, 1), dtype = np.uint8)
         """
         action_to_direction dict maps action to movement direction of robot arm.
         0,1,2,3,4,5 corresponds to front, back, left, right, up, down respectively
@@ -60,25 +60,50 @@ class PhilEnv(gym.Env):
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
+        '''
+        After first reset, loaded flag becomes True. Then, in subsequent resets URDF files do not need to be reloaded, only need to 
+        reset objects' base and orientations
+        '''
+        self.loaded = False
+
     def reset(self, seed = None, options = None):
-        p.resetSimulation()
-        p.setGravity(0,0,-9.81)
-        p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        super().reset(seed = seed)
+        if not self.loaded:
+            self.loaded = True
+            p.resetSimulation()
+            p.setGravity(0,0,-9.81)
+            p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
+            p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-        planeId = p.loadURDF('plane.urdf')
-        self.pandaId = p.loadURDF('franka_panda/panda.urdf', basePosition=[0,0,0.6],baseOrientation= p.getQuaternionFromEuler([0,0,0]),useFixedBase=True)
-        tableId = p.loadURDF('table/table.urdf', basePosition=[0.65/2,0,0], baseOrientation=p.getQuaternionFromEuler([0,0, 0]))
-        self.cubeId = p.loadURDF('cube_small.urdf', basePosition = [0.64, 0, 0.79], globalScaling = 0.6)
-        p.changeDynamics(self.cubeId, -1, 5) # changing the mass of the cube makes it more stationary when applying fixed constraint later on
-        neutral_joint_values = [0.00, 0.41, 0.00, -1.85, 0.00, 2.26, 0.79, 0.00, 0.00]
-        for i in range(len(neutral_joint_values)):  
-            p.resetJointState(self.pandaId, i, targetValue = neutral_joint_values[i]) # reset panda to neutral pos 
+            planeId = p.loadURDF('plane.urdf')
+            self.pandaId = p.loadURDF('franka_panda/panda.urdf', basePosition=[0,0,0.6],baseOrientation= p.getQuaternionFromEuler([0,0,0]),useFixedBase=True)
+            tableId = p.loadURDF('table/table.urdf', basePosition=[0.65/2,0,0], baseOrientation=p.getQuaternionFromEuler([0,0, 0]))
+            self.cubeId = p.loadURDF('cube_small.urdf', basePosition = [0.64, 0, 0.79], globalScaling = 0.6)
+            p.changeDynamics(self.cubeId, -1, 5) # changing the mass of the cube makes it more stationary when applying fixed constraint later on
+            neutral_joint_values = [0.00, 0.41, 0.00, -1.85, 0.00, 2.26, 0.79, 0.00, 0.00]
+            for i in range(len(neutral_joint_values)):  
+                p.resetJointState(self.pandaId, i, targetValue = neutral_joint_values[i]) # reset panda to neutral pos 
+            finger_joint_indices_values = [(9, 0.04), (10, 0.04)]
+            for i in range(len(finger_joint_indices_values)):
+                p.resetJointState(self.pandaId, finger_joint_indices_values[i][0], targetValue = finger_joint_indices_values[i][1]) # reset panda fingers
+            #self.rand_objId = p.loadURDF('random_urdfs/865/865.urdf', basePosition = [0.59,0, 0.61], globalScaling = 1) # some random object
+            col_boxId = p.createCollisionShape(p.GEOM_BOX, halfExtents = [0.03, 0.1, 0.01])
+            self.boxId = p.createMultiBody(1.0, col_boxId, basePosition = [0.59,0, 0.61], baseOrientation = p.getQuaternionFromEuler([0,0, 0]))
+            p.changeVisualShape(self.boxId, -1, rgbaColor = [0.26, 0.13, 0.02, 1.0]) # change color to dark brown
 
-        self.rand_objId = p.loadURDF('random_urdfs/865/865.urdf', basePosition = [0.59,0, 0.61]) # some random object
-        
-        cube_orn = p.getQuaternionFromEuler([0,0,0])
-        panda_cid = p.createConstraint(self.pandaId, 11, self.cubeId, -1, p.JOINT_FIXED, [0,0,0], [0.035, 0, -0.03], childFramePosition = [0,0,0], childFrameOrientation = cube_orn)
+            self.cube_orn = p.getQuaternionFromEuler([0,0,0])
+            panda_cid = p.createConstraint(self.pandaId, 11, self.cubeId, -1, p.JOINT_FIXED, [0,0,0], [0.035, 0, -0.03], childFramePosition = [0,0,0], childFrameOrientation = self.cube_orn)
+
+        else: # just reset base joint state/positions
+            neutral_joint_values = [0.00, 0.41, 0.00, -1.85, 0.00, 2.26, 0.79, 0.00, 0.00]
+            for i in range(len(neutral_joint_values)):  
+                p.resetJointState(self.pandaId, i, targetValue = neutral_joint_values[i]) # reset panda to neutral pos 
+            finger_joint_indices_values = [(9, 0.04), (10, 0.04)]
+            for i in range(len(finger_joint_indices_values)):
+                p.resetJointState(self.pandaId, finger_joint_indices_values[i][0], targetValue = finger_joint_indices_values[i][1]) # reset panda fingers
+            p.resetBasePositionAndOrientation(self.cubeId, posObj = [0.64, 0, 0.79], ornObj = p.getQuaternionFromEuler([0,0, 0]))
+            panda_cid = p.createConstraint(self.pandaId, 11, self.cubeId, -1, p.JOINT_FIXED, [0,0,0], [0.035, 0, -0.03], childFramePosition = [0,0,0], childFrameOrientation = self.cube_orn)
+
 
         observation = self._get_obs() # returns rgb array
         info = self._get_info() # returns cartesian dis
@@ -87,6 +112,35 @@ class PhilEnv(gym.Env):
             self.render()
             
         return observation, info
+
+    # def reset(self, seed = None, options = None):
+    #     super().reset(seed = seed)
+    #     p.resetSimulation()
+    #     p.setGravity(0,0,-9.81)
+    #     p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
+    #     p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+    #     planeId = p.loadURDF('plane.urdf')
+    #     self.pandaId = p.loadURDF('franka_panda/panda.urdf', basePosition=[0,0,0.6],baseOrientation= p.getQuaternionFromEuler([0,0,0]),useFixedBase=True)
+    #     tableId = p.loadURDF('table/table.urdf', basePosition=[0.65/2,0,0], baseOrientation=p.getQuaternionFromEuler([0,0, 0]))
+    #     self.cubeId = p.loadURDF('cube_small.urdf', basePosition = [0.64, 0, 0.79], globalScaling = 0.6)
+    #     p.changeDynamics(self.cubeId, -1, 5) # changing the mass of the cube makes it more stationary when applying fixed constraint later on
+    #     neutral_joint_values = [0.00, 0.41, 0.00, -1.85, 0.00, 2.26, 0.79, 0.00, 0.00]
+    #     for i in range(len(neutral_joint_values)):  
+    #         p.resetJointState(self.pandaId, i, targetValue = neutral_joint_values[i]) # reset panda to neutral pos 
+
+    #     self.rand_objId = p.loadURDF('random_urdfs/865/865.urdf', basePosition = [0.59,0, 0.61], globalScaling = 2) # some random object
+        
+    #     cube_orn = p.getQuaternionFromEuler([0,0,0])
+    #     panda_cid = p.createConstraint(self.pandaId, 11, self.cubeId, -1, p.JOINT_FIXED, [0,0,0], [0.035, 0, -0.03], childFramePosition = [0,0,0], childFrameOrientation = cube_orn)
+
+    #     observation = self._get_obs() # returns rgb array
+    #     info = self._get_info() # returns cartesian dis
+
+    #     if self.render_mode == 'human':
+    #         self.render()
+            
+    #     return observation, info
     
     def step(self, action):
         p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
@@ -105,24 +159,28 @@ class PhilEnv(gym.Env):
         observation = self._get_obs() # returns rgb array
         info = self._get_info() 
         
-        if info['distance'] <= 0.05:
+        if info['distance'] <= 0.08:
             # terminate because promixity reached
-            reward = 100
+            reward = 10000
             terminated = True 
-        elif 2 <= info['distance'] < 3:
-            reward = -np.exp(5*info['distance'])
-            terminated = False
-        elif info['distance'] < 2:
+        elif 0.08 < info['distance'] < 0.2:
             reward = 3*np.exp(1/(3*info['distance']))
             terminated = False
-        elif info['distance'] == 3:
+        elif 0.2 <= info['distance'] < 0.4:
+            reward = -np.exp(15*info['distance'])
+            terminated = False
+        elif info['distance'] >= 0.4:
             reward = -10000 # terminate because gripper was too far
             terminated = True  
-        else:
-            reward = -1 # small negative reward for every step 
-            terminated = False
+        # else:
+        #     reward = -1 # small negative reward for every step 
+        #     terminated = False
 
-        #print(f'Reward is: {reward}')
+        print(f'Reward is: {reward}')
+        print(f"Distance is: {info['distance']}")
+
+        if terminated:
+            print(' \n Terminated! Finally.')
 
         if self.render_mode == 'human':
             self.render()
@@ -133,7 +191,7 @@ class PhilEnv(gym.Env):
         return observation, reward, terminated, False, info
 
     def _get_info(self):
-        rand_obj_pos = p.getBasePositionAndOrientation(self.rand_objId)[0]
+        rand_obj_pos = p.getBasePositionAndOrientation(self.boxId)[0]
         end_effector_pos = p.getLinkState(self.pandaId, 11)[0]
         cart_dis = np.sqrt((rand_obj_pos[0] - end_effector_pos[0])**2+(rand_obj_pos[1] - end_effector_pos[1])**2+(rand_obj_pos[2] - end_effector_pos[2])**2)
         return {'distance': cart_dis}
@@ -142,7 +200,7 @@ class PhilEnv(gym.Env):
         '''
         Frame is originally 160x210x3 image
         Grayscale and threshold to downscale and transform input img to 84x84x1
-        CV2 takes in img as (height, width, channels). We will output obs as (channels, height, width)
+        CV2 takes in img as (height, width, channels).
         '''
     #     observation = np.zeros((self.num_channels, self.height, self.width), dtype=np.uint8)
     # #    for i in range(self.num_frames):
@@ -157,13 +215,15 @@ class PhilEnv(gym.Env):
         ret1, img_gray = cv2.threshold(img_gray, self.img_mask_thresh, 255, cv2.THRESH_BINARY) # if higher than img_mask_thresh, set to 255
         # print('Current array: \n')
         # print(img_gray)
-        img_gray = img_gray[:, :, None]
 
-        observation = np.zeros((self.num_channels, self.height, self.width), dtype=np.uint8)
-        channel_first_array = img_gray.transpose(2, 0, 1)
-        observation = np.array(channel_first_array, dtype=np.uint8)
+        #img_gray = img_gray[:, :, None] # Returns obs of shape [84, 84, 1]
+        img_gray = img_gray[None, :, :] # Returns obs of shape [1,84,84] - activate this line for sb3
 
-        return observation
+        # observation = np.zeros((self.num_channels, self.height, self.width), dtype=np.uint8)
+        # channel_first_array = img_gray.transpose(2, 0, 1)
+        # observation = np.array(channel_first_array, dtype=np.uint8)
+
+        return img_gray
 
     def render(self, mode = 'human'):
         cube_orn = p.getQuaternionFromEuler([0,0,0])
