3f8d83fd6e1de1e2e751768bc6cbeeb13ec053bb
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-09-25 21:08:53 +0800

    Changed max episode steps

--- gym-examples/gym_examples/__init__.py
+++ gym-examples/gym_examples/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
     id='gym_examples/PhilEnv-v1',
     entry_point='gym_examples.envs:PhilEnv',
-    max_episode_steps=100
+    max_episode_steps=200
 )
